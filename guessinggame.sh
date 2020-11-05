b73106a348e92fdb82b241956f2d3dca0d5635d2
Sofja96 <Sofi.Rubizova@yandex.ru>
2020-11-05 16:02:32 +0300

    добавлена проверка на ввод символов

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,24 +2,28 @@
 # File: guessinggame.sh
 
 countfile=$(ls -l | grep "^-" | wc -l)
+echo "How many files are in the current directory? Make a guess: " 
+
 
 function countfiles {
 	
-	while [[ $answer -ne $countfile ]]
-do
-	read -p "How many files are in the current directory? Make a guess: " answer
+	while [[ read ]]
 	
-				if [[  $answer =~ [A-Za-z] ]] 
+do
+	read answer
+
+	if [[  $answer =~ [A-Za-z] ]] || [[ $answer =~ [[:punct:]] ]]
 			then echo "You entered non-numeric, try again"
 		
-		elif [[ $answer -lt $countfile ]]
+	elif [[ $answer -lt $countfile ]]
 			then echo "The guess was too low, try again"
 	
 	elif [[ answer -gt countfile ]]
-		then echo "The guess was too high, try again"
+		    then echo "The guess was too high, try again"
 	
-	else echo "The guess is correct, Congratulations!"
-	
+		 else
+	 		echo "The guess is correct, Congratulations!"
+	exit
 	fi
 done
 
